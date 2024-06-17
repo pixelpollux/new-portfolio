a1648a4071f22e6d6559832e007a01629a12dd83
Tara Dunmore <terasmash@gmail.com>
2024-06-17 16:23:20 -0500

    adding old content back in in the form of react components

---
+++ src/components/little-sections/sec-contact.js
@@ -0,0 +1,52 @@
+function SecContact() {
+    return (
+        <div class="section-links" id="contact-link">contact section
+            <section id="contact-me">
+                <h2>Contact me!</h2>
+                <div id="socials" >
+                    
+                    <div id="iconbox" class="flex-container d-none d-lg-block">
+                        <a href="https://github.com/qualmless">
+                            <i class="flex-item fa fa-github fa-4x icon-3d"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/tarajdunmore/">
+                            <i class="flex-item fa fa-linkedin fa-4x icon-3d"></i>
+                        </a>
+                        <a href="mailto:me@taradunmore.com">
+                            <i class="flex-item far fa-envelope-open fa-4x icon-3d"></i>
+                        </a>
+                    </div>
+
+                    <div class="d-block d-lg-none">
+                        <ul>
+                            <li>
+                                <a href="https://www.linkedin.com/in/tarajdunmore/" target="_blank">
+                                    <button class="btn button_slide slide_right">
+                                        <i class="fab fa-linkedin"></i> LinkedIn
+                                    </button>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/qualmless" target="_blank">
+                                    <button class="btn button_slide slide_right">
+                                        <i class="fab fa-github"></i> GitHub
+                                    </button>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="mailto:me@taradunmore.com" target="_blank">
+                                    <button class="btn button_slide slide_right">
+                                        <i class="far fa-envelope"></i> Email
+                                    </button>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    )
+};
+
+export default SecContact;
