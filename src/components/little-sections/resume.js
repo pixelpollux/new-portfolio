8d7bd476fb2dce1396984d4b0803283da4c40e2e
Tara Dunmore <tara.dunmore@vml.com>
2024-06-26 11:18:28 -0500

    moved resume to seperate comp, navbar not working yet

---
+++ src/components/little-sections/resume.js
@@ -0,0 +1,22 @@
+export default function Resume() {
+
+    return (    
+        <div id="extended-bio" className="">
+            <div className='resume__job'>
+                <h3>
+                    Work Experience
+                </h3>
+                <h4 className='resume__comp-name'>
+                    Company Name
+                </h4>
+                <div className='resume__subtitle-section'>
+                    <div className='resume__job-title'>Job Title</div>
+                    <div className='resume__job-dates'>Dates</div>
+                </div>
+                <p className='resume__job-description'>
+                Cotton candy muffin bear claw bear claw fruitcake caramels tiramisu danish. Sugar plum powder gummies ice cream liquorice cookie cupcake dragée wafer. Apple pie topping jelly-o jelly-o pie carrot cake sweet.
+                </p>
+            </div>
+        </div>
+    );
+};
--- src/components/page-structure/navbar.js
+++ src/components/page-structure/navbar.js
@@ -2,19 +2,29 @@
 // import './App.css';
 // import '../App.css'
 import React, {useState} from 'react';
+import { useMediaQuery } from 'react-responsive';
 
-function Navbar() {
+export default function Navbar() {
 
-    const [isOpen, setIsOpen] = useState(true);
+    const isDesktopOrLaptop = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1224px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 992px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+    const [isOpen, setIsOpen] = useState(false);
 
     function handleButton(){
+        // if (isTabletOrMobile) {
+        //     return;
+        // }
         setIsOpen(!isOpen);
     };
 
     return (
         <nav className="navbar navbar-expand-lg sticky-top navbar-light shift">
             <div className="container">
-                <a className="navbar-brand" href="#" onClick={handleButton}>home</a>
+                <a className="navbar-brand" href="#">home</a>
                 <button 
                     onClick={handleButton}
                     className="navbar-toggler dropdown-button navbar-toggler-right" 
@@ -27,20 +37,20 @@ function Navbar() {
                 {isOpen &&
                     <div className={`navbar-collapse ${handleButton ? " " : " "}`} id="navbar-content">
                         <ul className="navbar-nav">
-                            <li className={`nav-item ${handleButton ? " " : ""}`} onClick={handleButton}>
+                            <li className={`nav-item ${handleButton ? "on" : "off"} ${isTabletOrMobile ? "<992" : ">992"}`} onClick={handleButton}>
                                 <a className="nav-link"  data-toggle=""  href="#about-me-link">about me</a>
                             </li>
-                            <li className="nav-item" onClick={handleButton}>
-                                <a className="nav-link" data-toggle="collapse" data-target="#navbar-collapse" href="#projects-link">projects</a>
+                            <li className="nav-item" onClick={() => isTabletOrMobile ? console.log("is tablet") : handleButton }>
+                                <a className="nav-link" data-toggle="collapse" data-target="#navbar-collapse" href="#skills-link">skills</a>
                             </li>
-                            <li className="nav-item" onClick={handleButton}>
+                            <li className="nav-item" onClick={() => isTabletOrMobile ? "": handleButton}>
+                                <a className="nav-link" data-toggle="collapse" data-target="#navbar-collapse" href="#resume-link">résumé</a>
+                            </li>
+                            {/* <li className="nav-item" onClick={() => isTabletOrMobile ? "": this.handleButton}>
                                 <a className="nav-link" data-toggle="collapse" data-target="#navbar-collapse" href="#certifications-link">certifications</a>
-                            </li>
-                            <li className="nav-item" onClick={handleButton}>
+                            </li> */}
+                            <li className="nav-item" onClick={() => isTabletOrMobile ? "": handleButton}>
                                 <a className="nav-link" data-toggle="collapse" data-target="#navbar-collapse" href="#contact-link">contact me</a>
-                            </li>
-                            <li className="nav-item" onClick={handleButton}>
-                                <a className="nav-link" target="_blank" data-toggle="collapse" data-target="#navbar-collapse" href="assets/resume322021.pdf">resumé</a>
                             </li>
                         </ul>
                     </div>
@@ -67,4 +77,4 @@ function Navbar() {
     // </div>
 
 
-export default Navbar;
+// export default Navbar;
